d5f05e973e532fb1142f8a710533f15903bd3db7
AntuanRokanten <anton_donntu@mail.ru>
2016-02-23 14:42:58 +0200

    ui of all activities created

---
+++ app/src/main/java/com/implemica/krokodeal/LaunchFragment.java
@@ -0,0 +1,19 @@
+package com.implemica.krokodeal;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * @author ant
+ */
+public class LaunchFragment extends Fragment {
+
+   @Override
+   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+      return inflater.inflate(R.layout.launch_fragment, container, false);
+   }
+}
